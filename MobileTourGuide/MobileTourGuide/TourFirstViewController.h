488468a9f96e66f08fc9c8a78626e00cfded3905
Scott Andrus <scott.andrus@vanderbilt.edu>
2012-02-16 13:16:22 -0600

    Created Xcode project for development.

---
+++ MobileTourGuide/MobileTourGuide/TourFirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  TourFirstViewController.h
+//  MobileTourGuide
+//
+//  Created by Scott Andrus on 2/16/12.
+//  Copyright (c) 2012 Vanderbilt University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TourFirstViewController : UIViewController
+
+@end
---
+++ MobileTourGuide/MobileTourGuide/TourSecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  TourSecondViewController.h
+//  MobileTourGuide
+//
+//  Created by Scott Andrus on 2/16/12.
+//  Copyright (c) 2012 Vanderbilt University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TourSecondViewController : UIViewController
+
+@end
